fc402b71e3955bc06447b938960b0a4a5eb8a7fa
dominic lizarraga <dominiclizarraga@hotmail.com>
2024-12-25 13:45:36 -0600

    Add files to try versions out

---
+++ version_3.3.4/string_mutation.rb
@@ -0,0 +1,19 @@
+def modify_strings
+  # Case 1: Direct string mutation
+  greeting = "Hello"
+  greeting << " World"
+  puts "Case 1 result: #{greeting}"
+
+  # Case 2: String mutation in collection
+  words = ["Ruby", "is", "fun"]
+  words.each { |word| word.upcase! }
+  puts "Case 2 result: #{words.join(' ')}"
+
+  # Case 3: String concatenation with +=
+  message = "Starting"
+  5.times { message += "." }
+  puts "Case 3 result: #{message}"
+end
+
+puts "Ruby Version: #{RUBY_VERSION}"
+modify_strings

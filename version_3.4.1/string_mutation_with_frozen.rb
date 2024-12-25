ad300b61b857cee9fbac629f62a36e54dfa7e91a
dominic lizarraga <dominiclizarraga@hotmail.com>
2024-12-25 14:13:00 -0600

    Add in-purpose code example

--- version_3.4.1/string_mutation_with_frozen.rb
+++ version_3.4.1/string_mutation_with_frozen.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+## this is the safe way, using dup, .map, and .reduce
+
 def modify_strings
   # Case 1: String modification using dup
   greeting = "Hello"
@@ -29,3 +31,27 @@ end
 
 puts "Ruby Version: #{RUBY_VERSION}"
 modify_strings
+
+
+# # this is done in purpose to show the error
+# the error happens when you try to modify a frozen string in place, 
+# but in your current code, you're already using the proper techniques to avoid these errors
+
+# def modify_strings
+#   # Let's try to modify strings directly
+#   greeting = "Hello"
+#   greeting << " World"  # This will raise FrozenError
+
+#   # Or try this
+#   name = "Ruby"
+#   name.upcase!  # This will also raise FrozenError
+# end
+
+# puts "Ruby Version: #{RUBY_VERSION}"
+# modify_strings
+
+## ERROR MESSAGE
+# version_3.4.1 git:(master) ruby -W:deprecated string_mutation_with_frozen.rb
+# Ruby Version: 3.4.1
+# string_mutation_with_frozen.rb:40:in 'Object#modify_strings': can't modify frozen String: "Hello" (FrozenError)
+# 	from string_mutation_with_frozen.rb:48:in '<main>'
